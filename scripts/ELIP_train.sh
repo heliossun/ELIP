966b0be9e2b5598d9f12602439c095c1e9d83653
Zachary Sun <sunguohao8895@gmail.com>
2024-01-27 13:16:09 -0500

    Update ELIP_train.sh

--- scripts/ELIP_train.sh
+++ scripts/ELIP_train.sh
@@ -1,5 +1,5 @@
 python -m torch.distributed.run --nproc_per_node=4 \
-train_retrieval.py \
---config configs/retrieval_coco_finetune-noEV.yaml \
---output_dir /path/to/output \
---seed 255
+    train_retrieval.py \
+    --config configs/retrieval_coco_finetune-noEV.yaml \
+    --output_dir /path/to/output \
+    --seed 255
